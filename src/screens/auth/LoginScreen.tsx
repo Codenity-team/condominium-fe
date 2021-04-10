fec74dc958cb9f088c2bb2d2f0200aaaa4ec73d3
Cesar Garcia <cesargarcia2194@gmail.com>
2021-04-10 19:51:42 -0400

    :construction: creation login and register screens

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,9 +1,80 @@
 import React from 'react'
+import { Avatar, Button, Container, CssBaseline, FormControlLabel, 
+    Grid, TextField, Typography, Link, Checkbox, Box  } from '@material-ui/core';
+import {LockOutlined } from '@material-ui/icons';
+import useStyleLogin from '../../styles/auth/LoginScreen-style';
+import { Copyright } from '../../components/Copyright';
+import { Link as LinkRoute} from 'react-router-dom';
+
 
 export const LoginScreen = () => {
+
+    const classes = useStyleLogin();
     return (
-        <div>
-            login
-        </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                        <Link variant="body2">
+                            Forgot password?
+                        </Link>
+                        </Grid>
+                        <Grid item>
+                        <Link variant="body2">
+                            <LinkRoute to="/auth/register">
+                                {"Don't have an account? Sign Up"}
+                            </LinkRoute>
+                        </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
     )
 }
--- src/screens/auth/RegisterScreen.tsx
+++ src/screens/auth/RegisterScreen.tsx
@@ -1,9 +1,102 @@
 import React from 'react'
+import { Avatar, Box, Button, Checkbox, Container, CssBaseline, Link, FormControlLabel, Grid, TextField, Typography } 
+from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+import { Link as LinkRoute} from 'react-router-dom';
+import useStylesRegister from '../../styles/auth/RegisterScreen-style'
+import { Copyright } from '../../components/Copyright';
 
 export const RegisterScreen = () => {
+
+    const classes = useStylesRegister();
     return (
-        <div>
-            Register
-        </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+                <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign up
+            </Typography>
+            <form className={classes.form} noValidate>
+                <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        autoComplete="fname"
+                        name="firstName"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="lname"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControlLabel
+                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                    />
+                </Grid>
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                Sign Up
+                </Button>
+                <Grid container justify="flex-end">
+                <Grid item>
+                    <Link variant="body2">
+                        <LinkRoute to="/auth/login">
+                            Already have an account? Sign in
+                        </LinkRoute>
+                    </Link>
+                </Grid>
+                </Grid>
+            </form>
+            </div>
+            <Box mt={5}>
+                <Copyright />
+            </Box>
+        </Container>
     )
 }
